6815af42b0e465da4b714167d49e0ca5aa815fc8
ung <11623560+ungung@user.noreply.gitee.com>
2024-07-09 23:59:14 +0800

    'commit'

--- create.cmd
+++ create.cmd
@@ -1,3 +1,5 @@
+@echo off
+
 chcp 65001
 cd/d %~dp0
 
@@ -5,11 +7,14 @@ rd /s /q docs
 md docs
 copy CNAME docs
 
+cls
+
 hugo -d docs
+echo.
+echo 根据上述Hugo编译日志判断是否编译成功，再决定是否上传远程仓库? (y/n)
+set /p ch=">"
 
-set /p ch=是否上传远程仓库? (1/0)
-
-if %ch%==1 (
+if %ch%==y (
 git add .
 git commit -m 'commit'
 git push -u origin master
